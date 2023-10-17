26fab851dbe09b3dc6025b9d95973fb9166457f9
Sumit986132 <148045235+Sumit986132@users.noreply.github.com>
2023-10-17 16:08:29 +0000

    function_array

---
+++ basic/function_array.js
@@ -0,0 +1,14 @@
+const array1 = [100 ,500, 1000,2000 ,2500 ]
+function pickSecondArray(getarray){
+    console.log(getarray[1])
+    return getarray
+}
+pickSecondArray(array1)
+
+function calItem(...num1){
+    console.log(num1)
+    return num1
+
+}
+calItem(200, 300 ,300 ,400 ,555)
+
