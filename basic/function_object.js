658531f23cb46f82f17634adfcf19447ecaaf442
Sumit986132 <148045235+Sumit986132@users.noreply.github.com>
2023-10-17 15:54:37 +0000

    function.object

---
+++ basic/function_object.js
@@ -0,0 +1,17 @@
+const user ={
+    name : "sam",
+    email : "sam123@gmail.com",
+    inLogged : false
+}
+function handleObject(anyobject){
+    console.log(`hi ${anyobject.name} check our email ${anyobject.email}`)
+    return handleObject
+
+}
+const login = {
+    name : "ritik",
+    email : "ritik23@gmail.com"
+
+}
+console.log(login)
+console.log(user)
