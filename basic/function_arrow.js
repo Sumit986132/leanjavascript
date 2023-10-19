f101a0b700eb8a5fc2247c28d72197d1828c4a6c
Sumit986132 <148045235+Sumit986132@users.noreply.github.com>
2023-10-19 17:54:48 +0000

    function_arrow

---
+++ basic/function_arrow.js
@@ -0,0 +1,13 @@
+const addTwoNumber = (num1,num2) => {
+    return num1 + num2
+} 
+console.log(addTwoNumber(3,5));
+
+const userid = (num1,num2) => ({username : "sumeet"})
+
+console.log(userid(3,5));
+
+
+
+
+    
